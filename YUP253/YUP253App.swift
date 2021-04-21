8f41337739b4c52fad011cc5725e71e2582e0fd6
Jason <jdlacy@gmail.com>
2021-04-21 13:57:03 -0700

    Initial Commit

---
+++ YUP253/YUP253App.swift
@@ -0,0 +1,17 @@
+//
+//  YUP253App.swift
+//  YUP253
+//
+//  Created by Student Account on 4/21/21.
+//
+
+import SwiftUI
+
+@main
+struct YUP253App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YUP253Tests/YUP253Tests.swift
@@ -0,0 +1,33 @@
+//
+//  YUP253Tests.swift
+//  YUP253Tests
+//
+//  Created by Student Account on 4/21/21.
+//
+
+import XCTest
+@testable import YUP253
+
+class YUP253Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
