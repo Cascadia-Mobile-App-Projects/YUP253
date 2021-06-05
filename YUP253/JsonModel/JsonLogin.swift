8dad283fd097dd512795b3499b000f823eaadd5d
Stephen Gomez-Fox <stephen@gomezfox.net>
2021-06-04 23:14:12 -0700

    Updates JSON structs to use Auth

--- YUP253/JsonModel/JsonLogin.swift
+++ YUP253/JsonModel/JsonLogin.swift
@@ -8,34 +8,34 @@
 import Foundation
 
 struct LoginDetails : Codable {
-    var username : String = "anonymous"
+    var username : String? = "anonymous"
     var password: String?
     var authtoken: String?
 }
 
 // user details returned from the server
 struct UserDetails : Codable {
-    var id : UUID
-    var username : String
-    var first_name : String
-    var last_name : String
+    var user_id : UUID?
+    var username : String?
+    var first_name : String?
+    var last_name : String?
     var birthdate : Date?
     var headline: String?
     var about : String?
     var avatar : URL?
-    var permissions: Int
 }
 
 // sections returned under "data"
 struct LoginResponseData : Codable {
+    var token: String?
+    var permissions: [Auth]?
     var user : UserDetails?
-    var permissions: Int?
-    var token: String
 }
 
 // general response: success, message, data
 struct LoginResponse : Codable {
-    var success : Bool = false
+    var success : Bool? = false
     var message : String?
     var data : LoginResponseData?
 }
+
